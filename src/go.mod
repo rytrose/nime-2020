6fcf159fae93e1c928fa7b54d128c19841915ae4
Ryan Rose <rytrose@gmail.com>
2020-04-23 07:27:41 -0400

    initial commit, barebones firebase, ws, mongo

---
+++ src/go.mod
@@ -0,0 +1,12 @@
+module github.com/rytrose/nime2020
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/gorilla/websocket v1.4.2
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/sirupsen/logrus v1.5.0
+	go.mongodb.org/mongo-driver v1.3.2
+)
---
+++ src/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+	r := gin.Default()
+
+	// Configure templates and static files
+	r.LoadHTMLGlob("client/templates/*")
+	r.Static("/js", "client/js")
+
+	// Root app
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Wow, title!",
+		})
+	})
+
+	// Websocket handler
+	r.GET("/ws", func(c *gin.Context) {
+		wsHandler(c.Writer, c.Request)
+	})
+
+	// Test db
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo:27017"))
+	if err != nil {
+		log.Fatalf("DB connect error: %s", err)
+	}
+	ctx, _ = context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatalf("DB ping error: %s", err)
+	}
+
+	r.Run(":8000")
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatalf("Unable to upgrate ws request: %s", err)
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, []byte(string(message)+"!"))
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
