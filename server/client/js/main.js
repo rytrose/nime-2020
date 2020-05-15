a0fe69e35f45661113fc2770bc00e9ee36c2e9d9
Ryan Rose <rytrose@gmail.com>
2020-05-14 21:46:52 -0400

    basic login

--- server/client/js/main.js
+++ server/client/js/main.js
@@ -1,180 +1,208 @@
 const db = firebase.firestore();
 const rtdb = firebase.database();
 const auth = firebase.auth();
-const ui = new firebaseui.auth.AuthUI(auth);
 
 let users = db.collection('users');
 let rooms = db.collection('rooms');
 
 let socket;
+let userDoc;
 
-const uiConfig = {
-    'callbacks': {
-        'signInSuccessWithAuthResult': (authResult, _) => {
-            if (authResult.user) {
-                console.log("user signed in:", authResult.user);
-            }
-            if (authResult.additionalUserInfo) {
-                console.log("is new user:", authResult.additionalUserInfo.isNewUser);
-            }
+// Once DOM is ready
+$(() => {
+    // Sign in logic
+    $("#signInForm").submit((e) => {
+        e.preventDefault();
+        let email = $("#signInEmail").val();
+        let password = $("#signInPassword").val();
+        auth.signInWithEmailAndPassword(email, password)
+            .catch((error) => {
+                switch (error.code) {
+                    case "auth/invalid-email":
+                        alert("Invalid email.");
+                        break;
+                    case "auth/user-disabled":
+                        alert("User disabled.");
+                        break;
+                    case "auth/user-not-found":
+                        $("#signUpEmail").val(email);
+                        $("#signUpPassword").val(password);
+                        $("#signIn").hide();
+                        $("#signUp").show();
+                        break;
+                    case "auth/wrong-password":
+                        alert("Incorrect password.");
+                        break;
+                    default:
+                        alert(error.message);
+                }
+            });
+    });
+    $("#signInLink").click(() => {
+        $("#signUp").hide();
+        $("#signIn").show();
 
-            // Do not redirect
-            return false;
-        },
-        'signInFailure': (error) => {
-            console.log("unable to sign in:", error);
-        },
-        'uiShown': () => {
-            $("#loading").hide();
-        }
-    },
-    'signInOptions': [
-        // Only allow email/password sign-in
-        {
-            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
-        }
-    ],
-    'credentialHelper': firebaseui.auth.CredentialHelper.NONE
-}
-ui.disableAutoSignIn();
+    });
 
+    // Sign up logic
+    $("#signUpForm").submit((e) => {
+        e.preventDefault();
+        let email = $("#signUpEmail").val();
+        let password = $("#signUpPassword").val();
+        let displayName = $("#signUpDisplayName").val();
 
-// Create ephemeral user
-// firebase.auth().signInAnonymously().catch((error) => {
-//     console.log("error signing in anonymously:", error);
-// });
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                userCredential.user.updateProfile({
+                    displayName: displayName
+                })
+                    .then(() => { 
+                        console.log("successfully set display name");
+                        $("#user").text(`${userCredential.user.displayName} (${userCredential.user.email})`);
+                        $("#content").show();
+                    })
+                    .catch((e) => { console.log(error.message) });
+            })
+            .catch((error) => {
+                switch (error.code) {
+                    case "auth/email-already-in-use":
+                        alert("Email already in use.");
+                        break;
+                    case "auth/invalid-email":
+                        alert("Invalid email.");
+                        break;
+                    case "auth/weak-password":
+                        alert("Password too weak.");
+                        break;
+                    default:
+                        alert(error.message);
+                }
+            });
+    });
+    $("#signUpLink").click((e) => {
+        e.preventDefault();
+
+        $("#signIn").hide();
+        $("#signUp").show();
+    });
+
+    // Sign out logic
+    $("#signOut").click((e) => {
+        e.preventDefault();
+
+        auth.signOut();
+        $("#content").hide();
+        $("#signIn").show();
+    });
+});
 
 // Once logged in
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         console.log("user signed in", user);
-        $("#content").show();
-        $("#content").append(`<p>Hello ${user.displayName} (${user.email})!</p>`);
-        // // Add user to firestore
-        // users.add({}).then((userDoc) => {
-        //     let userID = userDoc.id;
-        //     $("#welcome").html(`Welcome, ${userID}!`);
-        //     console.log("added user");
+        $("#signIn").hide();
+        $("#signUp").hide();
+        if (user.displayName) {
+            $("#user").text(`${user.displayName} (${user.email})`);
+            $("#content").show();
+        }
 
-        //     // Set RTDB online key
-        //     let presenceRef = rtdb.ref(`/status/${userID}`)
-        //     presenceRef.set('online').then(() => {
-        //         console.log("added user presence ref");
-        //     }).catch((error) => {
-        //         console.log("error adding user presence ref:", error);
-        //     });
-            
-        //     // On RTDB set offline for deletion from firestore
-        //     $(window).on('beforeunload', (e) => {
-        //         presenceRef.set('offline')
-        //             .then(() => console.log("set user offline"))
-        //             .catch((error) => console.error("unable to set user offline:", error));
-        //         e.returnValue = "";
-        //         return "";
-        //     });
+        // Retain reference to user doc in firestore
+        userDoc = users.doc(user.uid);
 
-        //     // Connect to server via websocket
-        //     socket = new Socket(`${location.host}/ws`);
+        // Connect to server via websocket
+        socket = new Socket(`${location.host}/ws`);
 
-        //     // Set up callbacks
-        //     socket.register("operationUpdate", (m) => {
-        //         $("#operations").append(`<p>${JSON.stringify(m.operation)}</p>`);
-        //     });
-        //     socket.register("clearState", (m) => {
-        //         $("#operations").empty();
-        //     });
-        //     socket.register("numMembersUpdate", (m) => {
-        //         $("#numMembers").text(m.numMembers);
-        //     });
+        // Set up callbacks
+        socket.register("operationUpdate", (m) => {
+            $("#operations").append(`<p>${JSON.stringify(m.operation)}</p>`);
+        });
+        socket.register("clearState", (m) => {
+            $("#operations").empty();
+        });
+        socket.register("numMembersUpdate", (m) => {
+            $("#numMembers").text(m.numMembers);
+        });
 
-        //     // Announce this user 
-        //     socket.addEventListener("open", () => {
-        //         socket.send({
-        //             "id": uuidv4(),
-        //             "type": "announce",
-        //             "userID": userID
-        //         });
-        //     });
-        // }).catch((error) => {
-        //     console.log("error adding user:", error);
-        // });
+        // Announce this user 
+        socket.addEventListener("open", () => {
+            socket.send({
+                "id": uuidv4(),
+                "type": "announce",
+                "userID": user.uid
+            });
+        });
     } else {
         console.log("authStateChanged, no user signed in");
-        $("#loading").hide();
-        ui.start('#firebaseui-auth-container', uiConfig);
+        $("#signIn").show();
     }
 });
 
-let signOut = () => {
-    auth.signOut();
-    $("#content").hide();
-};
-
 // Sync rooms
-// rooms.where('active', '==', true).onSnapshot((snapshot) => {
-//     if (!snapshot.size) console.log("No rooms.");
+rooms.where('active', '==', true).onSnapshot((snapshot) => {
+    if (!snapshot.size) console.log("No rooms.");
 
-//     snapshot.docChanges().forEach(function (change) {
-//         let roomName = change.doc.id;
-//         let roomData = change.doc.data();
-//         if (change.type === 'removed') $(`#r${change.doc.id}`).remove();
-//         else if(change.type === 'added') {
-//             $("#rooms").append(`
-//             <div id="r${roomName}">
-//                 <h3 id="r${roomName}_name">${roomName}</h3>
-//                 <p id="r${roomName}_description">${roomData.description}</p>
-//                 <button id="r${roomName}_enter">Enter ${roomName}</button>
-//                 <hr>
-//             </div>
-//             `);
-//             $(`#r${roomName}_enter`).click(() => {
-//                 socket.sendWithResponse({
-//                     "id": uuidv4(),
-//                     "type": "enterRoom",
-//                     "roomName": roomName
-//                 })
-//                     .then(res => {
-//                         $("#currentRoom").html(`
-//                         <h3>Welcome to room ${res.roomDoc.RoomName}!</h3>
-//                         <p><span id="numMembers">${res.roomDoc.NumMembers}</span> people in this room</p>
-//                         <button id="operate">Commit operation</button>
-//                         <button id="exitRoom">Exit ${res.roomDoc.RoomName}</button>
-//                         <div id="operations">Most recent operations:</div>
-//                         `)
-//                         for(let operation of res.operations) {
-//                             $("#operations").append(`<p>${JSON.stringify(operation)}</p>`)
-//                         }
-//                         $("#operate").click(() => {
-//                             let operation = {
-//                                 "operationType": "foo",
-//                                 "data": Math.random()
-//                             };
-//                             $("#operations").append(`<p>${JSON.stringify(operation)}</p>`)
-//                             socket.send({
-//                                 "type": "operation",
-//                                 "roomName": roomName,
-//                                 "operation": operation
-//                             });
-//                         });
-//                         $("#exitRoom").click(() => socket.sendWithResponse({
-//                                 "id": uuidv4(),
-//                                 "type": "exitRoom",
-//                                 "roomName": roomName,
-//                             })
-//                             .then(() => {
-//                                 $("#currentRoom").empty();
-//                                 $("button[id$=_enter]").prop("disabled", false);
-//                             })
-//                             .catch(e => {
-//                                 console.error("unable to exit room:", e)
-//                             })
-//                         )
-//                     })
-//                     .catch(e => console.log("unable to enter room:", e));
-//                 $("button[id$=_enter]").prop("disabled", true);
-//             })
-//         } else {
-//             $(`#r${roomName}_description`).html(roomData.description);
-//         }
-//     });
-// });
+    snapshot.docChanges().forEach(function (change) {
+        let roomName = change.doc.id;
+        let roomData = change.doc.data();
+        if (change.type === 'removed') $(`#r${change.doc.id}`).remove();
+        else if(change.type === 'added') {
+            $("#rooms").append(`
+            <div id="r${roomName}">
+                <h3 id="r${roomName}_name">${roomName}</h3>
+                <p id="r${roomName}_description">${roomData.description}</p>
+                <button id="r${roomName}_enter">Enter ${roomName}</button>
+                <hr>
+            </div>
+            `);
+            $(`#r${roomName}_enter`).click(() => {
+                socket.sendWithResponse({
+                    "id": uuidv4(),
+                    "type": "enterRoom",
+                    "roomName": roomName
+                })
+                    .then(res => {
+                        $("#currentRoom").html(`
+                        <h3>Welcome to room ${res.roomDoc.RoomName}!</h3>
+                        <p><span id="numMembers">${res.roomDoc.NumMembers}</span> people in this room</p>
+                        <button id="operate">Commit operation</button>
+                        <button id="exitRoom">Exit ${res.roomDoc.RoomName}</button>
+                        <div id="operations">Most recent operations:</div>
+                        `)
+                        for(let operation of res.operations) {
+                            $("#operations").append(`<p>${JSON.stringify(operation)}</p>`)
+                        }
+                        $("#operate").click(() => {
+                            let operation = {
+                                "operationType": "foo",
+                                "data": Math.random()
+                            };
+                            $("#operations").append(`<p>${JSON.stringify(operation)}</p>`)
+                            socket.send({
+                                "type": "operation",
+                                "roomName": roomName,
+                                "operation": operation
+                            });
+                        });
+                        $("#exitRoom").click(() => socket.sendWithResponse({
+                                "id": uuidv4(),
+                                "type": "exitRoom",
+                                "roomName": roomName,
+                            })
+                            .then(() => {
+                                $("#currentRoom").empty();
+                                $("button[id$=_enter]").prop("disabled", false);
+                            })
+                            .catch(e => {
+                                console.error("unable to exit room:", e)
+                            })
+                        )
+                    })
+                    .catch(e => console.log("unable to enter room:", e));
+                $("button[id$=_enter]").prop("disabled", true);
+            })
+        } else {
+            $(`#r${roomName}_description`).html(roomData.description);
+        }
+    });
+});
